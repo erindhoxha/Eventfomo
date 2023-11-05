9884f72b8ab82070f395adbac5048602206abde0
erindhoxha <erind.cbh@gmail.com>
2023-11-05 15:50:16 +1100

    Styling

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -24,25 +24,15 @@ const Card = ({ label, title, description, subtext }: CardProps) => {
       {title}
      </CardTitle>
     )}
-    {/* <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    strokeWidth="2"
-    className="h-4 w-4 text-muted-foreground"
-   >
-    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-   </svg> */}
    </CardHeader>
 
    <CardContent>
     {description && (
      <div className="text-2xl font-bold mb-1">{description}</div>
     )}
-    {subtext && <p className="text-xs text-muted-foreground">{subtext}</p>}
+    {subtext && (
+     <p className="text-xs text-muted-foreground max-w-xs">{subtext}</p>
+    )}
    </CardContent>
   </UICard>
  );
--- app/components/SubscribeForm/SubscribeForm.tsx
+++ app/components/SubscribeForm/SubscribeForm.tsx
@@ -5,77 +5,77 @@ import * as z from 'zod';
 
 import { Button } from '@/components/ui/button';
 import {
-  Form as UIForm,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormMessage,
+ Form as UIForm,
+ FormControl,
+ FormDescription,
+ FormField,
+ FormItem,
+ FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useForm } from 'react-hook-form';
 import { Icons } from '@/components/ui/icons';
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: 'Username must be at least 2 characters.',
-  }),
+ username: z.string().min(2, {
+  message: 'Username must be at least 2 characters.',
+ }),
 });
 
 const Form = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: '',
-    },
-  });
+ const form = useForm<z.infer<typeof formSchema>>({
+  resolver: zodResolver(formSchema),
+  defaultValues: {
+   username: '',
+  },
+ });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
+ function onSubmit(values: z.infer<typeof formSchema>) {
+  // Do something with the form values.
+  // ✅ This will be type-safe and validated.
+  console.log(values);
+ }
 
-  return (
-    <UIForm {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full lg:w-max">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <div>
-                  <p className="text-sm mb-1.5">Join 100+ users</p>
-                  <div className="flex flex-col sm:flex-row max-w-sm space-y-2 sm:space-y-0">
-                    <Input
-                      className="lg:min-w-[250px]"
-                      placeholder="Your best email"
-                      {...field}
-                    />
-                    <Button
-                      type="submit"
-                      className="lg:ml-1"
-                      disabled={form.formState.isSubmitting}
-                    >
-                      {form.formState.isSubmitting && (
-                        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                      )}
-                      Subscribe
-                    </Button>
-                  </div>
-                </div>
-              </FormControl>
-              <FormDescription>
-                We will send you the events in this email.
-              </FormDescription>
-              <FormMessage className="text-destructive" />
-            </FormItem>
-          )}
-        />
-      </form>
-    </UIForm>
-  );
+ return (
+  <UIForm {...form}>
+   <form onSubmit={form.handleSubmit(onSubmit)} className="w-full lg:w-max">
+    <FormField
+     control={form.control}
+     name="username"
+     render={({ field }) => (
+      <FormItem>
+       <FormControl>
+        <div>
+         <p className="text-sm mb-1.5">Join 100+ users</p>
+         <div className="flex flex-col sm:flex-row max-w-sm space-y-2 sm:space-y-0">
+          <Input
+           className="lg:min-w-[250px]"
+           placeholder="Your best email"
+           {...field}
+          />
+          <Button
+           type="submit"
+           className="lg:ml-1"
+           disabled={form.formState.isSubmitting}
+          >
+           {form.formState.isSubmitting && (
+            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+           )}
+           Subscribe
+          </Button>
+         </div>
+        </div>
+       </FormControl>
+       <FormDescription className="max-w-[230px]">
+        We will send you events on this email 24 hours before tournaments start.
+       </FormDescription>
+       <FormMessage className="text-destructive" />
+      </FormItem>
+     )}
+    />
+   </form>
+  </UIForm>
+ );
 };
 
 export default Form;
