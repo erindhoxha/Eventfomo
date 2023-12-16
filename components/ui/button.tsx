ac567e5f76e8c032761af4c4e4635c4c5a73e397
erindhoxha <erind.cbh@gmail.com>
2023-12-16 23:57:58 +1100

    Revert primary changes

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
  {
   variants: {
    variant: {
-    default: 'bg-primary/80 text-primary-foreground shadow hover:bg-primary/90',
+    default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
     success: 'bg-success text-success-foreground shadow-sm hover:bg-success/90',
     destructive:
      'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
