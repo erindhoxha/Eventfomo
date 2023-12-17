9e71a9b9b05d0ba8ef76ab9a206e47a7dce61dab
erindhoxha <erind.cbh@gmail.com>
2023-12-18 02:08:44 +1100

    Idk anymore

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const nextConfig = {
-  trailingSlash: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+ trailingSlash: true,
+ images: {
+  unoptimized: true,
+ },
 };
 module.exports = nextConfig;
