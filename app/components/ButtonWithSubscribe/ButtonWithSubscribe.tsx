06f11076a50fdd1325395e31d8d323b74d9c16cb
erindhoxha <hello@erindhoxha.dev>
2024-02-06 02:32:00 +1100

    Subscribing fix

--- app/components/ButtonWithSubscribe/ButtonWithSubscribe.tsx
+++ app/components/ButtonWithSubscribe/ButtonWithSubscribe.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import supabase from "@/supabase";
+import { User } from "@supabase/supabase-js";
+import { useState } from "react";
 
 const ButtonWithSubscribe = ({
   user,
@@ -9,30 +11,55 @@ const ButtonWithSubscribe = ({
   gameId,
   gameName,
 }: {
-  user: any;
+  user: User | undefined;
   subscribed: boolean;
   gameId: string;
   gameName: string;
 }) => {
-  const subscribe = async () => {
-    const data =
-      user?.id &&
-      (await supabase
-        .from("user_game_subscriptions")
-        .insert({ user_id: user?.id, game_id: gameId }));
+  const [loading, setLoading] = useState(false);
+  const [subscribedState, setSubscribed] = useState(subscribed);
 
-    if (data && data.error) {
-      console.error("Error subscribing to chess events", data.error);
+  const subscribe = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    let data;
+    if (user?.id) {
+      if (subscribedState) {
+        // If the user is already subscribed, delete the subscription
+        data = await supabase
+          .from("user_game_subscriptions")
+          .delete()
+          .eq("user_id", user.id)
+          .eq("game_id", gameId);
+      } else {
+        // If the user is not subscribed, insert the subscription
+        data = await supabase
+          .from("user_game_subscriptions")
+          .insert({ user_id: user.id, game_id: gameId });
+      }
     }
+
+    setLoading(false);
+    if (data && data.error) {
+      console.error("Error updating subscription", data.error);
+      return;
+    }
+    // Toggle the subscribed state
+    setSubscribed(!subscribedState);
   };
 
   return (
     <Button
-      variant={subscribed ? "success" : "default"}
+      variant={subscribedState ? "success" : "default"}
       className="w-full md:w-auto"
       onClick={() => subscribe()}
     >
-      {subscribed ? "Subscribed ✓" : `Subscribe to ${gameName} events`}
+      {loading
+        ? "Loading..."
+        : subscribedState
+        ? "Subscribed ✓"
+        : `Subscribe to ${gameName} events`}
     </Button>
   );
 };
--- app/games/wow/page.tsx
+++ app/games/wow/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
 };
 
 export default async function WowPage() {
+  console.log("Running!");
+
   return (
     <GameTemplate
       gameName="World of Warcraft"
