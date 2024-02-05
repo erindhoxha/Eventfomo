6cc1b12ccf81f4006893f1db1457a21a9ad9a571
erindhoxha <hello@erindhoxha.dev>
2024-02-05 15:27:49 +1100

    Added footer

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="mt-6 mb-6 text-xs text-white p-4 text-center">
+      <p>&copy; 2024 Eventfomo</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,28 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { ThemeProvider } from '@/components/context/theme-provider';
+import "./globals.css";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/context/theme-provider";
+import Footer from "./components/Footer/Footer";
 
 export const metadata: Metadata = {
- title: 'Eventfomo',
- description: 'Never miss out on any gaming events anymore.',
+  title: "Eventfomo",
+  description: "Never miss out on any gaming events anymore.",
 };
 
 export default async function RootLayout(props: any) {
- return (
-  <html lang="en" suppressHydrationWarning>
-   <body className="bg-gradient-to-r bg-hero-pattern">
-    <ThemeProvider
-     attribute="class"
-     defaultTheme="system"
-     enableSystem
-     disableTransitionOnChange
-    >
-     {props.directoryListing}
-     {props.children}
-    </ThemeProvider>
-   </body>
-  </html>
- );
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-gradient-to-r bg-hero-pattern">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {props.directoryListing}
+          {props.children}
+        </ThemeProvider>
+        <Footer />
+      </body>
+    </html>
+  );
 }
