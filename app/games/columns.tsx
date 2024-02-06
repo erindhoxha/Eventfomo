cf93747fc4d6dd1cb023f3df7bede08e5ceaffe3
erindhoxha <hello@erindhoxha.dev>
2024-02-06 19:19:51 +1100

    Finished MVP version

--- app/games/columns.tsx
+++ app/games/columns.tsx
@@ -1,58 +1,65 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { Database } from '@/database.generated.types';
-import { ColumnDef } from '@tanstack/react-table';
-import { ArrowUpDown } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import { Database } from "@/database.generated.types";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { format } from "date-fns";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
 export type Games = {
- id: number;
- name?: string;
- game?: Database['public']['Enums']['Game'];
- prizePool?: string;
- date?: string;
- createdAt?: Date;
+  id: number;
+  name?: string;
+  game?: Database["public"]["Enums"]["Game"];
+  prizePool?: string;
+  date?: string;
+  createdAt?: Date;
 };
 
 export const columns: ColumnDef<Games>[] = [
- {
-  accessorKey: 'name',
-  header: 'Name',
- },
- {
-  accessorKey: 'prizePool',
-  header: ({ column }) => {
-   return (
-    <Button
-     style={{
-      paddingLeft: 0,
-     }}
-     variant="none"
-     onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-    >
-     Prize Pool
-     <ArrowUpDown className="ml-2 h-4 w-4" />
-    </Button>
-   );
+  {
+    accessorKey: "name",
+    header: "Name",
   },
- },
- {
-  accessorKey: 'date',
-  header: ({ column }) => {
-   return (
-    <Button
-     style={{
-      paddingLeft: 0,
-     }}
-     variant="none"
-     onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-    >
-     Event Date
-     <ArrowUpDown className="ml-2 h-4 w-4" />
-    </Button>
-   );
+  {
+    accessorKey: "starts_at",
+    header: ({ column }) => {
+      return (
+        <Button
+          style={{
+            paddingLeft: 0,
+          }}
+          variant="none"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Start date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: (asd) => {
+      const date = asd.getValue() as string;
+      return format(new Date(date || ""), "dd/MM/yyyy");
+    },
   },
- },
+  {
+    accessorKey: "ends_at",
+    header: ({ column }) => {
+      return (
+        <Button
+          style={{
+            paddingLeft: 0,
+          }}
+          variant="none"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          End date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: (asd) => {
+      const date = asd.getValue() as string;
+      return format(new Date(date || ""), "dd/MM/yyyy");
+    },
+  },
 ];
