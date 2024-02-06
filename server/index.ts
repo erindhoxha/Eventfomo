c6432a218640ab312d2eca3c0a17493727cbd91a
erindhoxha <hello@erindhoxha.dev>
2024-02-06 20:26:51 +1100

    Puppeteer wip

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,19 @@
 const puppeteer = require("puppeteer");
+import { Browser } from "puppeteer";
+
+const url = "https://books.toscrape.com/";
+
+const main = async () => {
+  const browser: Browser = await puppeteer.launch({
+    headless: false,
+    // defaultViewport: null,
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  await browser.close();
+};
+
+main();
